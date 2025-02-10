fa9b0016414226f571175316f1d89506dd311114
John Wang <johnz.wang@outlook.com>
2025-02-10 16:03:08 +0100

    Add to_string

--- prolog.c
+++ prolog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 
@@ -45,11 +46,59 @@ struct Term* substitute(struct Term* outer, int variable, struct Term* inner) {
   return 0;
 }
 
+char* to_string(struct Term* term) {
+  char* s = malloc(1000);
+  if (term->variable) {
+    sprintf(s, "%d", term->variable);
+  } else if (term->functor) {
+    sprintf(s, "%s(", term->functor->name);
+    for (int i = 0; i < term->functor->arity; i++) {
+      char* rec = to_string(term->functor->args[i]);
+      strcat(s, rec);
+      if (i != term->functor->arity - 1) {
+        strcat(s, ", ");
+      }
+      free(rec);
+    }
+    strcat(s, ")");
+  }
+  return s;
+}
+
+struct Term * create_variable(int i) {
+  
+  struct Term *t1 = malloc(sizeof *t1);
+  t1->variable = i;
+  t1->functor = NULL;
+
+  return t1;
+}
+
+struct Term * create_functor(char* name, int arity) {
+  struct Term *t3 = malloc(sizeof *t3);
+  t3->functor = malloc(sizeof (struct Functor));
+  t3->functor->arity = arity;
+  t3->functor->name = name;
+  return t3;
+  
+}
+
+
 struct Term* unify(struct Term* a, struct Term* b) {
 
   return 0;
 }
 
 int main() {
+  struct Term *t1 = create_variable(1);
+  struct Term *t2 = create_variable(2);
+  struct Term *t3 = create_functor("my_name", 2);
+  t3->functor->args[0] = t1;
+  t3->functor->args[1] = t1;
+  
+
+  char* s = to_string(t3);
+
+  printf("%s", s);
   
 }
