968468c4b0c0a3c8c55853628c525e1e08dee6f2
John Wang <johnz.wang@outlook.com>
2025-04-16 15:05:33 +0200

    Format haskell

--- lisp_haskell.hs
+++ lisp_haskell.hs
@@ -1,7 +1,7 @@
 import Control.Applicative
 import Control.Monad
-import Control.Monad.Trans.State.Lazy
 import Control.Monad.Trans.Class
+import Control.Monad.Trans.State.Lazy
 import Data.Char
 import Data.List
 import Data.Maybe
@@ -16,7 +16,7 @@ parseChar :: Parser Char
 --     f [] = Nothing
 --     f (x : xs) = Just (x, xs)
 parseChar = do
-  (x:xs) <- get
+  (x : xs) <- get
   put xs
   pure x
 
@@ -54,12 +54,12 @@ parseList =
               List e <$> parseExpList
          in nonEmpty <|> pure (Atom "Nil")
    in do
-     parseMany parseSpace
-     '(' <- parseChar
-     list <- parseExpList
-     parseMany parseSpace
-     ')' <- parseChar
-     pure list
+        parseMany parseSpace
+        '(' <- parseChar
+        list <- parseExpList
+        parseMany parseSpace
+        ')' <- parseChar
+        pure list
 
 validAtomChar :: Parser Char
 validAtomChar = do
@@ -74,13 +74,12 @@ parseAtom = do
 
 printExp :: Exp -> String
 printExp (Atom s) = s
-printExp (List l r) = let
-  printList (Atom "Nil") = ""
-  printList (Atom s) = " . " ++ s
-  printList (List ll (Atom "Nil")) = printExp ll
-  printList (List ll lr) = printExp ll ++ " " ++ printList lr
-  in
-    "(" ++ printList (List l r) ++ ")"
+printExp (List l r) =
+  let printList (Atom "Nil") = ""
+      printList (Atom s) = " . " ++ s
+      printList (List ll (Atom "Nil")) = printExp ll
+      printList (List ll lr) = printExp ll ++ " " ++ printList lr
+   in "(" ++ printList (List l r) ++ ")"
 
 car :: Exp -> Either String Exp
 car (Atom x) = Left $ "Cannot car " ++ x
@@ -93,9 +92,7 @@ cdr (List _ y) = Right y
 cons :: Exp -> Exp -> Exp
 cons = List
 
-
 nil = Atom "Nil"
-
 
 type Env = [(String, Exp)]
 
@@ -103,47 +100,40 @@ envLookup :: Env -> String -> Maybe Exp
 envLookup = flip lookup
 
 readEval :: String -> Env -> Either String (Exp, Env)
-readEval str env = 
+readEval str env =
   case parse str of
     Just parsed -> runStateT (eval parsed) env
     Nothing -> Left "Parse Fail"
 
-
 repl :: Env -> IO Env
-repl env = do 
+repl env = do
   input <- readLn
   let Just p = parse input
   let output = eval p
   let Right (evalOutput, newEnv) = runStateT output env
   print $ printExp $ evalOutput
-  repl newEnv 
-  
+  repl newEnv
 
-eval :: Exp  -> StateT Env (Either String) Exp
+eval :: Exp -> StateT Env (Either String) Exp
 eval exp = case exp of
-    Atom x -> do 
-      env <- get
-      lift $ Right $ fromMaybe (Atom x) (envLookup env x)
-    List (Atom "car") (Atom _) -> lift $ Left "Car called with no arguments"
-    List (Atom "car") (List arg (Atom "Nil")) -> do
-      e1 <- eval arg
-      lift (car e1) 
-    List (Atom "car") (List _ _) -> lift $ Left "Car with too many arguments"
-    List (Atom "cons") (List arg1 (List arg2 (Atom "Nil"))) -> do
-      e1 <- eval arg1
-      e2 <- eval arg2
-      pure $ cons e1 e2
-    List (Atom "cons") (_) -> lift . Left $ "Arguments for cons incorrect"
-    List (Atom "cdr") (List arg (Atom "Nil")) -> do
-      e1 <- eval arg
-      lift (cdr e1)
-    List (Atom "cdr") (_) -> lift . Left $ "Incorrect arguments for cdr"
-    List (Atom "def") (List (Atom var) body) -> do
-      env <- get
-      put ((var,body):env)
-      return body
-
-
-
-    	
-
+  Atom x -> do
+    env <- get
+    lift $ Right $ fromMaybe (Atom x) (envLookup env x)
+  List (Atom "car") (Atom _) -> lift $ Left "Car called with no arguments"
+  List (Atom "car") (List arg (Atom "Nil")) -> do
+    e1 <- eval arg
+    lift (car e1)
+  List (Atom "car") (List _ _) -> lift $ Left "Car with too many arguments"
+  List (Atom "cons") (List arg1 (List arg2 (Atom "Nil"))) -> do
+    e1 <- eval arg1
+    e2 <- eval arg2
+    pure $ cons e1 e2
+  List (Atom "cons") (_) -> lift . Left $ "Arguments for cons incorrect"
+  List (Atom "cdr") (List arg (Atom "Nil")) -> do
+    e1 <- eval arg
+    lift (cdr e1)
+  List (Atom "cdr") (_) -> lift . Left $ "Incorrect arguments for cdr"
+  List (Atom "def") (List (Atom var) body) -> do
+    env <- get
+    put ((var, body) : env)
+    return body
