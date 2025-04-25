bf13b202dc3a3e7437ac2c9422e4c539d88765fd
John Wang <johnz.wang@outlook.com>
2025-04-25 15:43:36 +0200

    Add store to haskell

--- lisp_haskell.hs
+++ lisp_haskell.hs
@@ -6,7 +6,7 @@ import Data.Char
 import Data.List
 import Data.Maybe
 
-data Exp = Atom String | List Exp Exp deriving (Show)
+data Exp = Lambda Env Exp | Atom String | List Exp Exp deriving (Show)
 
 type Parser = StateT String Maybe
 
@@ -76,6 +76,7 @@ parseAtom = do
 
 printExp :: Exp -> String
 printExp (Atom s) = s
+printExp (Lambda _ _) = "<<lambda exp>>"
 printExp (List l r) =
   let printList (Atom "Nil") = ""
       printList (Atom s) = " . " ++ s
@@ -96,12 +97,18 @@ cons = List
 
 nil = Atom "Nil"
 
-type Env = [(String, Exp)]
+type Env = [(String, Int)]
 
-envLookup :: Env -> String -> Maybe Exp
-envLookup = flip lookup
+envLookup :: (Env,Store) -> String -> Maybe Exp
+envLookup (env, store) string = do
+  index <- lookup string env
+  listToMaybe $ drop index store
 
-readEval :: String -> StateT Env (Either String) Exp
+type Store = [Exp]
+
+type ProgramState = StateT (Env, Store) (Either String) 
+
+readEval :: String -> ProgramState Exp
 readEval str = case parse str of 
   Just x -> return x
   Nothing -> lift (Left "parse error") 
@@ -109,30 +116,28 @@ readEval str = case parse str of
   
   
 
-repl :: Env -> IO Env
-repl env = do
-  input <- readLn
-  let Just p = parse input
-  let output = eval p
-  let Right (evalOutput, newEnv) = runStateT output env
-  print $ printExp evalOutput
-  repl newEnv
+-- repl :: Env -> IO Env
+-- repl env = do
+--   input <- readLn
+--   case runState
+--   let Just p = parse input
+--   let output = eval p
+--   let Right (evalOutput, newEnv) = runStateT output env
+--   print $ printExp evalOutput
+--   repl newEnv
 
-patternMatch :: Exp -> Exp -> StateT Env (Either String) Env
-patternMatch (Atom "Nil") (Atom "Nil") = get
-patternMatch (Atom v) exp = do
-  env <- get
-  return ((v, exp):env)
-patternMatch (List patternX patternXs) (List listX listXs) = do
-  e1 <- patternMatch patternX patternXs
-  put e1
-  patternMatch patternXs listXs
-patternMatch pattern values = lift $ Left $ "Cannot match " ++ printExp pattern ++ "with " ++ printExp values
+patternMatch :: Exp -> Exp -> ProgramState Env
+patternMatch = undefined
+-- patternMatch (Atom "Nil") (Atom "Nil") = get
+-- patternMatch (Atom v) exp = do
+--   env <- get
+--   return ((v, exp):env)
+-- patternMatch (List patternX patternXs) (List listX listXs) = do
+--   e1 <- patternMatch patternX patternXs
+--   patternMatch patternXs listXs
+-- patternMatch pattern values = lift $ Left $ "Cannot match " ++ printExp pattern ++ "with " ++ printExp values
   
-  
-
-
-eval :: Exp -> StateT Env (Either String) Exp
+eval :: Exp -> ProgramState Exp
 eval exp = case exp of
   Atom x -> do
     env <- get
@@ -151,21 +156,33 @@ eval exp = case exp of
     e1 <- eval arg
     lift (cdr e1)
   List (Atom "cdr") _ -> lift . Left $ "Incorrect arguments for cdr"
-  List (Atom "def") (List (Atom var) body) -> do
-    env <- get
-    put ((var, body) : env)
+  List (Atom "def") (List (Atom var) (List body (Atom "Nil"))) -> do
+    (env, store) <- get
+    put ((var, length store) : env, store ++ [body])
     return body
-  List (Atom "lambda") rest -> return (List (Atom "lambda") rest)
-  List (List (Atom "lambda") (List pattern body)) outer -> do
+  List (Atom "def") _ -> lift $ Left "Incorrect arguments for def"
+  (List (Atom "lambda") body) -> do
+    (env, _) <- get
+    return $ Lambda env body
+  List (Lambda oldEnv (List pattern body)) outer -> do
     eOuter <- eval outer
-    patternMatch pattern body
+    matchedEnv <- patternMatch pattern body
     get <- undefined
     undefined
-    
 programs :: [(String, String)]
-programs = [("(car (cons 1 2))", "1"), ("(cdr (cons 1 2))", "2")]
+programs = 
+  [
+   ("(car (cons 1 2))", "1")
+   , ("(cdr (cons 1 2))", "2")
+   , ("(def x 12)", "12")
+   , ("x", "12")
+   , ("(((lambda x (lambda y x)) 1) 2)", "1")
+  ]
+runMultiple :: [String] -> ProgramState [Exp]
+runMultiple = mapM (readEval >=> eval)
 
-
-
-runMultiple :: [String] -> StateT Env (Either String) [Exp]
-runMultiple = mapM (\x -> readEval x >>= eval)
+compareTest :: Either String [(String, String, String)]
+compareTest = let p = map fst programs
+                  expect = map snd programs
+                  actual = evalStateT (runMultiple p) ([], []) in
+                  zip3 p expect  . map printExp <$> actual
